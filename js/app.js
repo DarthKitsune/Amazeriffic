05e79ab2bd6ff3de7f6e692cf3335dca5bbec190
DarthKitsune <msu.v.mahanov@gmail.com>
2015-08-05 23:49:07 +0500

    something

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
 var main = function () {
     "use strict";
-    var makeTabActive = function (tabNumber) {
-// сконструируем селектор из tabNubmer
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(".tabs span").removeClass("active");
-        $(tabSelector).addClass("active");
-    };
-    $(".tabs a:nth-child(1)").on("click", function () {
-        makeTabActive(1);
-        return false;
+    var toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты"
+    ];
+    $(element).on("click", function () {
+        var $element = $(element),
+            $content;
+        $(".tabs a span").removeClass("active");
+        $element.addClass("active");
+        $("main .content").empty();
+        if ($element.parent().is(":nth-child(1)")) {
+            console.log("Щелчок на первой вкладке!");
+        } else if ($element.parent().is(":nth-child(2)")) {
+            $content = $("<ul>");
+            toDos.forEach(function (todo) {
+                $content.append($("<li>").text(todo));
+            });
+            $("main .content").append($content);
+        } else if ($element.parent().is(":nth-child(3)")) {
+            console.log("Щелчок на третьей вкладке!");
+        }
     });
-    $(".tabs a:nth-child(2)").on("click", function () {
-        makeTabActive(2);
-        return false;
+    $(".tabs a span").toArray().forEach(function(element) {
+        $(element).on("click", function () {
+            $(".tabs a span").removeClass("active");
+            element.addClass("active");
+            $("main .content").empty();
+        });
     });
-    $(".tabs a:nth-child(3)").on("click", function () {
-        makeTabActive(3);
-        return false;
-    });
+    //return false;
 };
 
 $(document).ready(main());
