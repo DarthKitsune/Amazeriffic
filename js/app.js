f2aa65c4a4dbb8aaea0e09eec0d9e8a1c924913b
DarthKitsune <msu.v.mahanov@gmail.com>
2015-08-05 23:04:52 +0500

    DRY

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-/**
- * Created by hp on 05.08.2015.
- */
+var main = function () {
+    "use strict";
+    var makeTabActive = function (tabNumber) {
+// сконструируем селектор из tabNubmer
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(".tabs span").removeClass("active");
+        $(tabSelector).addClass("active");
+    };
+    $(".tabs a:nth-child(1)").on("click", function () {
+        makeTabActive(1);
+        return false;
+    });
+    $(".tabs a:nth-child(2)").on("click", function () {
+        makeTabActive(2);
+        return false;
+    });
+    $(".tabs a:nth-child(3)").on("click", function () {
+        makeTabActive(3);
+        return false;
+    });
+};
+
+$(document).ready(main());
