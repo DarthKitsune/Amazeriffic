6aa37b08298c1f710ada1f816a263f1ada955ff0
DarthKitsune <msu.v.mahanov@gmail.com>
2015-08-05 21:30:32 +0500

    app.js -> DRY

--- chapter5-js/app.js
+++ chapter5-js/app.js
@@ -4,26 +4,23 @@
 
 var main = function () {
     "use strict";
-    $(".comment-input button").on("click", function (event) {
-        var $new_comment;
+    var addCommentFromInputBox = function () {
+        var new_comment;
         if($(".comment-input input").val() != "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
+            new_comment = $("<p>").text($(".comment-input input").val());
+            new_comment.hide();
+            $(".comments").append(new_comment);
+            new_comment.fadeIn();
             $(".comment-input input").val("");
         }
+    };
+
+    $(".comment-input button").on("click", function () {
+        addCommentFromInputBox();
     });
     $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode == 13) {
-            var $new_comment;
-            if ($(".comment-input input").val() != "") {
-                $new_comment = $("<p>").text($(".comment-input input").val());
-                $new_comment.hide();
-                $(".comments").append($new_comment);
-                $new_comment.fadeIn();
-                $(".comment-input input").val("");
-            }
+            addCommentFromInputBox();
         }
     });
 };
