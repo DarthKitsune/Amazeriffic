61b031796f913517262fd51a86dec42ebe2427a0
DarthKitsune <msu.v.mahanov@gmail.com>
2015-08-05 20:21:26 +0500

    app.js -> hide text

--- chapter5-js/app.js
+++ chapter5-js/app.js
@@ -8,7 +8,9 @@ var main = function () {
         var $new_comment;
         if($(".comment-input input").val() != "") {
             $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
             $(".comments").append($new_comment);
+            $new_comment.fadeIn();
             $(".comment-input input").val("");
         }
     });
@@ -17,7 +19,9 @@ var main = function () {
             var $new_comment;
             if ($(".comment-input input").val() != "") {
                 $new_comment = $("<p>").text($(".comment-input input").val());
+                $new_comment.hide();
                 $(".comments").append($new_comment);
+                $new_comment.fadeIn();
                 $(".comment-input input").val("");
             }
         }
