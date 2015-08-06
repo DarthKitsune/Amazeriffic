ab239812c854b83715176b1a0dcb12f7f89fccc9
DarthKitsune <msu.v.mahanov@gmail.com>
2015-08-06 19:49:07 +0500

    load images from flickr

--- chapter5-bridge/js/app.js
+++ chapter5-bridge/js/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by hp on 05.08.2015.
  */
+
+var main = function () {
+    var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
+        "tags=cat&format=json&jsoncallback=?";
+    $.getJSON(url, function (flickrResponse) {
+    //пока мы просто выводим ответ в консоль
+        flickrResponse.items.forEach(function (photo) {
+           //console.log(photo.media.m);
+            var img = $("<img>");
+            img.attr("src", photo.media.m);
+            $("main .photos").append(img);
+        });
+    });
+}
+
+$(document).load(main());
